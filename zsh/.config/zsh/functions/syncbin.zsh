2528c9381c072c82a52e1efd789119e3bafb7679
Dylan Barker <dthomasbarker@gmail.com>
2021-05-23 20:09:14 +0100

    some updates
    fix
    
    patch
    
    fix
    
    fix

--- zsh/.config/zsh/functions/syncbin.zsh
+++ zsh/.config/zsh/functions/syncbin.zsh
@@ -1,11 +1,11 @@
 syncbin() {
     local dir="${1:-$HOME/scripts}"
-    [ -z $1 ] && { rm -r ~/bin; mkdir ~/bin }
+    [ -z $1 ] && find ~/.local/bin -type l -delete
 
     for i in $dir/*; do
         ! [ -d "$i" ] && {
             chmod +x $i
-            ln -s $i ~/bin
+            ln -s $i ~/.local/bin
         } || syncbin "$i"
     done
 }
